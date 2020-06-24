aadafd73c9f5e3a6d6c78076fe08d26ab9814342
haribrata123 <haribrata.nayak@gmail.com>
2020-06-24 11:31:14 +0530

    commit the object

---
+++ MyApp1/src/main/java/comm/ofss/MyApp1/App.java
@@ -0,0 +1,13 @@
+package comm.ofss.MyApp1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
